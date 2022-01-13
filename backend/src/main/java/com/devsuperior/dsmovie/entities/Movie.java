6306a2dc9ea6106fb99720a348b2f4a8380cda4a
Gustavo Brandao <gustavojcbrandao@gmail.com>
2022-01-12 21:53:23 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Movie.java
@@ -0,0 +1,73 @@
+package com.devsuperior.dsmovie.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_movie")
+public class Movie {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	private Double score;
+	private Integer count;
+	private String image;
+	
+	public Movie () {
+		
+	}
+
+	public Movie(Long id, String tittle, Double score, Integer count, String image) {
+		this.id = id;
+		this.title = tittle;
+		this.score = score;
+		this.count = count;
+		this.image = image;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTittle() {
+		return title;
+	}
+
+	public void setTittle(String tittle) {
+		this.title = tittle;
+	}
+
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
+	}
+
+	public Integer getCount() {
+		return count;
+	}
+
+	public void setCount(Integer count) {
+		this.count = count;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+	
+}
